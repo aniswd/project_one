55c23704c1b03d77a6364e45e90d11963eaa5d8b
Anis Sheikh <119504504+aniswd@users.noreply.github.com>
2023-05-29 21:41:32 +0600

    Update plugin.js

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -3,7 +3,7 @@ $(function() {
 
 
     // OWL CAROUSEL resort
-    $("#resort .owl-carousel").owlCarousel({
+    $("#photoGal .owl-carousel").owlCarousel({
             loop: true,
             margin: 10,
             autoplay: true,
@@ -43,5 +43,38 @@ $(function() {
                 }
             }
         });
+
+    // Magnific popup start
+
+    var magnifPopup = function() {
+        $('._popup_img').magnificPopup({
+            type: 'image',
+            removalDelay: 300,
+            mainClass: 'mfp-with-zoom',
+            gallery: {
+                enabled: true
+            },
+            zoom: {
+                enabled: true, // By default it's false, so don't forget to enable it
+
+                duration: 300, // duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function
+
+                // The "opener" function should return the element from which popup will be zoomed in
+                // and to which popup will be scaled down
+                // By defailt it looks for an image tag:
+                opener: function(openerElement) {
+                    // openerElement is the element on which popup was initialized, in this case its <a> tag
+                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
+            }
+        });
+    };
+
+    // Call the functions
+    magnifPopup();
+
+    // Magnific popup end
 
 });
